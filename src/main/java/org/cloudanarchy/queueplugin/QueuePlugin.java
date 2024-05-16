2e9a6d4696d9df3daddcc4aae52d482628a2d9a3
Florian Hübner <fhuebner@posteo.de>
2024-05-16 22:53:46 +0200

    refactor

--- src/main/java/org/cloudanarchy/queueplugin/QueuePlugin.java
+++ src/main/java/org/cloudanarchy/queueplugin/QueuePlugin.java
@@ -8,20 +8,17 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerAdvancementDoneEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QueuePlugin extends JavaPlugin implements Listener {
 
-    public static final int X = 420;
-    public static final int Z = 69;
-    public static final int Y = 1337;
+    private static Location location;
 
     @Override
     public void onEnable() {
+        location = new Location(getServer().getWorlds().get(0), 0, 140, 0);
+
         getServer().setDefaultGameMode(GameMode.SPECTATOR);
         getServer().setSpawnRadius(32);
         getServer().getServerTickManager().setTickRate(1);
@@ -30,20 +27,20 @@ public final class QueuePlugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(eventCanceler, this);
         ProtocolLibrary.getProtocolManager().addPacketListener(eventCanceler);
 
-        Location location = new Location(getServer().getWorlds().get(0), X, Y, Z);
         getServer().getScheduler().runTaskTimer(this, () -> {
             for (Player player : getServer().getOnlinePlayers()) {
-                player.teleport(location);
-                player.setAllowFlight(true);
-                player.setFlying(true);
-                player.setInvisible(true);
-                player.setInvulnerable(true);
-                player.setGameMode(GameMode.SPECTATOR);
+                // vanish
                 for (Player p : getServer().getOnlinePlayers()) {
                     if (player.equals(p)) continue;
                     player.hidePlayer(this, p);
                     p.hidePlayer(this, player);
                 }
+                // properties
+                player.setGameMode(GameMode.SPECTATOR);
+                player.setAllowFlight(true);
+                player.setFlying(true);
+                player.setInvisible(true);
+                player.setInvulnerable(true);
             }
         }, 0, 0);
     }
@@ -51,11 +48,12 @@ public final class QueuePlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent ev) {
         ev.joinMessage(Component.empty());
+        ev.getPlayer().teleport(location, PlayerTeleportEvent.TeleportCause.PLUGIN);
     }
 
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent ev) {
-        ev.setRespawnLocation(new Location(getServer().getWorlds().get(0), X, Y, Z));
+        ev.setRespawnLocation(location);
     }
 
     @EventHandler
